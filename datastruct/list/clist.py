c9058322792a03fd8c7fa7fb7440f1e1509463e1
zhengxiaowai <h1x2y3awalm@gmail.com>
2018-09-10 23:39:55 +0800

    Feat: 双向循环链表

--- datastruct/list/clist.py
+++ datastruct/list/clist.py
@@ -55,13 +55,79 @@ class CircularDoublyList:
         self.__delitem__(pos)
 
     def __getitem__(self, pos):
-        pass
+        if pos < 0 or pos > self._length:
+            raise PositionError("pos error, 0 < pos < {}".format(self._length))
+
+        if pos == 0:
+            return self._head
+        elif pos == self._length:
+            return self.tail
+        else:
+            current_node = self._head
+            for _ in range(pos):
+                current_node = current_node.next
+            return current_node
 
     def __setitem__(self, pos, node):
-        pass
+        if not isinstance(node, DNode):
+            raise TypeError("node must a DNode")
+
+        if self._head is None and self._tail is None:
+            # insert a empty list
+            self._head = node
+            self._tail = node
+
+            self._head.prev, self._tail.next = self._tail, self._head
+        else:
+            if pos == 0:
+                # insert head
+                node.next = self.head
+                self.head.prev = node
+                self._head = node
+
+                self._head.prev, self._tail.next = self._tail, self._head
+            elif pos == self._length:
+                # insert tail
+                self._tail.next = node
+                node.prev = self._tail
+                self._tail = node
+
+                self._head.prev, self._tail.next = self._tail, self._head
+            else:
+                # insert middle
+                current_node = self.search(pos)
+                node.next = current_node
+                node.prev = current_node.prev
+                current_node.prev.next = node
+                current_node.prev = node
+
+        self._length += 1
 
     def __delitem__(self, pos):
-        pass
+        if self._head is None and self._tail is None:
+            return
+
+        current_node = self.search(pos)
+        if current_node is self._head:
+            if current_node.next is None:
+                # only one node
+                self._head = None
+                self._tail = None
+            else:
+                current_node.next.prev = None
+                self._head = current_node.next
+
+                self._head.prev, self._tail.next = self._tail, self._head
+        elif current_node is self._tail:
+            current_node.prev.next = None
+            self._tail = current_node.prev
+
+            self._head.prev, self._tail.next = self._tail, self._head
+        else:
+            current_node.prev.next = current_node.next
+            current_node.next.prev = current_node.prev
+
+        self._length -= 1
 
     def values(self):
         values = []
--- tests/test_clist.py
+++ tests/test_clist.py
@@ -39,3 +39,49 @@ class TestDoublyList(unittest.TestCase):
         self.assertIs(cl.tail.prev.prev, node1)
         self.assertIs(cl.tail.prev.prev.prev, node0)
         self.assertIs(cl.tail.prev.prev.prev.prev, node3)
+
+    def test_insert_clist(self):
+        node0 = DNode(0)
+        node1 = DNode(1)
+        node2 = DNode(2)
+        node3 = DNode(3)
+        cl = CircularDoublyList()
+
+        cl.insert(0, node0)
+        self.assertEqual(len(cl), 1)
+        self.assertIs(cl.head, node0)
+        self.assertIs(cl.tail, node0)
+        self.assertIs(cl.head.prev, cl.tail)
+        self.assertIs(cl.tail.next, cl.head)
+
+        cl.insert(0, node1) # node1 node0
+        self.assertEqual(len(cl), 2)
+        self.assertIs(cl.head, node1)
+        self.assertIs(cl.head.prev, cl.tail)
+        self.assertIs(cl.head.next, node0)
+        self.assertIs(cl.head.next.prev, node1)
+        self.assertIs(cl.tail, node0)
+        self.assertIs(cl.tail.next, cl.head)
+        self.assertIs(cl.tail.prev, node1)
+        self.assertIs(cl.tail.prev.next, node0)
+        self.assertIs(cl.head.prev, cl.tail)
+        self.assertIs(cl.tail.next, cl.head)
+
+        cl.insert(2, node2) # node1 node0 node2
+        self.assertEqual(len(cl), 3)
+        self.assertIs(cl.head, node1)
+        self.assertIs(cl.tail.next, cl.head)
+        self.assertIs(cl.tail, node2)
+        self.assertIs(cl.tail.prev, node0)
+        self.assertIs(cl.tail.prev.next, node2)
+        self.assertIs(cl.head.prev, cl.tail)
+        self.assertIs(cl.tail.next, cl.head)
+
+        cl.insert(2, node3)  # node1 node0 node3 node2
+        self.assertEqual(len(cl), 4)
+        self.assertIs(cl.tail.prev, node3)
+        self.assertIs(cl.tail.prev.next, node2)
+        self.assertIs(cl.tail.prev.prev, node0)
+        self.assertIs(cl.tail.prev.prev.next, node3)
+        self.assertIs(cl.head.prev, cl.tail)
+        self.assertIs(cl.tail.next, cl.head)
