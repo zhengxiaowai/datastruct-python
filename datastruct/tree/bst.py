682a22e585aa6d68e764b3c21819abad4008fa25
zhengxiaowai <h1x2y3awalm@gmail.com>
2018-09-19 23:40:12 +0800

    Feat: 二叉搜索树 Test: 二叉搜索树

---
+++ datastruct/tree/bst.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+"""
+二叉查找树
+"""
+
+
+class BitNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def __repr__(self):
+        return "Node({!r})".format(self.value)
+
+
+def check_type(ins, ins_type):
+    if isinstance(ins, ins_type):
+        raise TypeError("need the instance of {}".format(ins_type))
+
+
+def is_bst(root):
+    pass
+
+
+def minimum(root):
+    return minimum(root.left) if root.left else root
+
+
+def maximum(root):
+    return minimum(root.right) if root.right else root
+
+
+def successor(root, node):
+    """ 后继既是右子树的最小值 """
+    pass
+
+
+def predecessor(root, node):
+    """ 前驱既是左子树的最大值 """
+    pass
+
+
+def pre_order(root, res=None):
+    if root is None:
+        return []
+    if res is None:
+        res = []
+
+    res.append(root.value)
+    pre_order(root.left, res)
+    pre_order(root.right, res)
+
+    return res
+
+
+def in_order(root, res=None):
+    if root is None:
+        return []
+    if res is None:
+        res = []
+
+    in_order(root.left, res)
+    res.append(root.value)
+    in_order(root.right, res)
+
+    return res
+
+
+def post_order(root, res=None):
+    if root is None:
+        return []
+    if res is None:
+        res = []
+
+    post_order(root.left, res)
+    post_order(root.right, res)
+    res.append(root.value)
+
+    return res
+
+
+def recur_search(root, value):
+    if root is None:
+        return None
+
+    if root.value == value:
+        return root
+
+    if value < root.value:
+        return recur_search(root.left, value)
+    else:
+        return recur_search(root.right, value)
+
+
+def recur_insert(root, value):
+    if root.value == value:
+        return
+
+    if value < root.value:
+        # go to left
+        if root.left:
+            return recur_insert(root.left, value)
+        else:
+            root.left = BitNode(value)
+            return
+    else:
+        # go to right
+        if root.right:
+            return recur_insert(root.right, value)
+        else:
+            root.right = BitNode(value)
+            return
+
+
+def recur_delete(root, value):
+    if root is None:
+        return
+
+    if value < root.value:
+        root.left = recur_delete(root.left, value)
+
+    if value > root.value:
+        root.right = recur_delete(root.right, value)
+
+    if value == root.value:
+        if root.left is None:
+            return root.right
+
+        if root.right is None:
+            return root.left
+
+        min_node = root.right
+        p = None
+        while min_node.left is not None:
+            p = min_node
+            min_node = min_node.left
+
+        min_node.left = root.left
+        if p:
+            p.left = min_node.right
+            min_node.right = root.right
+
+        return min_node
+
+
+class BST:
+    def __init__(self, value=None):
+        self._root = BitNode(value)
+
+    @property
+    def root(self):
+        return self._root
+
+    def insert(self, value):
+        if self._root:
+            recur_insert(self._root, value)
+        else:
+            self._root = BitNode(value)
+
+    def search(self, value):
+        return recur_search(self._root, value)
+
+    def delete(self, value):
+        return recur_delete(self._root, value)
+
+    def pre_order(self):
+        return pre_order(self._root)
+
+    def in_order(self):
+        return in_order(self._root)
+
+    def post_order(self):
+        return post_order(self._root)
---
+++ tests/tree/bst.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import unittest
+
+from datastruct.tree.bst import pre_order, in_order, post_order, \
+    recur_insert, recur_search, recur_delete, BitNode
+
+
+class RecurBST(unittest.TestCase):
+    def test_pre_order(self):
+        n1 = BitNode(10)
+        n2 = BitNode(5)
+        n3 = BitNode(15)
+        n4 = BitNode(3)
+        n5 = BitNode(13)
+        n6 = BitNode(16)
+
+        n1.left, n1.right = n2, n3
+        n2.left, n2.right = n4, None
+        n3.left, n3.right = n5, n6
+
+        self.assertEqual([10, 5, 3, 15, 13, 16], pre_order(n1))
+
+    def test_in_order(self):
+        n1 = BitNode(10)
+        n2 = BitNode(5)
+        n3 = BitNode(15)
+        n4 = BitNode(3)
+        n5 = BitNode(13)
+        n6 = BitNode(16)
+
+        n1.left, n1.right = n2, n3
+        n2.left, n2.right = n4, None
+        n3.left, n3.right = n5, n6
+        self.assertEqual([3, 5, 10, 13, 15, 16], in_order(n1))
+
+    def test_post_order(self):
+        n1 = BitNode(10)
+        n2 = BitNode(5)
+        n3 = BitNode(15)
+        n4 = BitNode(3)
+        n5 = BitNode(13)
+        n6 = BitNode(16)
+
+        n1.left, n1.right = n2, n3
+        n2.left, n2.right = n4, None
+        n3.left, n3.right = n5, n6
+        self.assertEqual([3, 5, 13, 16, 15, 10], post_order(n1))
+
+    def test_insert(self):
+        n1 = BitNode(10)
+
+        recur_insert(n1, 5)
+        recur_insert(n1, 15)
+        recur_insert(n1, 3)
+        recur_insert(n1, 13)
+        recur_insert(n1, 16)
+
+        self.assertEqual(n1.value, 10)
+        self.assertEqual(n1.left.value, 5)
+        self.assertEqual(n1.left.left.value, 3)
+        self.assertEqual(n1.right.value, 15)
+        self.assertEqual(n1.right.left.value, 13)
+        self.assertEqual(n1.right.right.value, 16)
+
+    def test_search(self):
+        n1 = BitNode(10)
+        n2 = BitNode(5)
+        n3 = BitNode(15)
+        n4 = BitNode(3)
+        n5 = BitNode(13)
+        n6 = BitNode(16)
+
+        n1.left, n1.right = n2, n3
+        n2.left, n2.right = n4, None
+        n3.left, n3.right = n5, n6
+
+        self.assertIs(recur_search(n1, 10), n1)
+        self.assertIs(recur_search(n1, 5), n2)
+        self.assertIs(recur_search(n1, 15), n3)
+        self.assertIs(recur_search(n1, 3), n4)
+        self.assertIs(recur_search(n1, 13), n5)
+        self.assertIs(recur_search(n1, 16), n6)
+
+    def test_delete(self):
+        n1 = BitNode(20)
+        n2 = BitNode(10)
+        n3 = BitNode(30)
+        n4 = BitNode(5)
+        n5 = BitNode(15)
+        n6 = BitNode(2)
+        n7 = BitNode(12)
+        n8 = BitNode(13)
+
+        n1.left, n1.right = n2, n3
+        n2.left, n2.right = n4, n5
+        n4.left, n4.right = None, n6
+        n5.left, n5.right = n7, None
+        n7.left, n7.right = None, n8
+
+        recur_delete(n1, 10)
+        self.assertEqual([5, 2, 12, 13, 15, 20, 30], in_order(n1))
